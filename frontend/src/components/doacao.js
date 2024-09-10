fc4da7b740acbf8f70882458e5e9039dde24353c
Nuno <nunotavoravieira@gmail.com>
2024-09-10 12:59:28 -0300

    mudanças na página de doação

--- frontend/src/components/doacao.js
+++ frontend/src/components/doacao.js
@@ -1,11 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../App.css';
 
 const Doacao = () => {
+  const [item, setItem] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Lógica para enviar os dados do formulário
+    console.log('Item:', item);
+  };
+
   return (
-    <div className="doacao-container">
-      <h1>Faça sua Doação</h1>
-      <p>Teste</p>
+    <div className='doacao-page'>
+      <div className="doacao-container">
+        <div className='doacao-left'>
+          <h1>Faça sua Doação</h1>
+          <form onSubmit={handleSubmit}>
+            <div className="form-group">
+              <label htmlFor="doacao">Item:</label>
+              <input
+                type="text"
+                id="doacao"
+                value={item}
+                onChange={(e) => setItem(e.target.value)}
+                required
+              />
+            </div>
+
+            <button type="submit">Doar</button>
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
